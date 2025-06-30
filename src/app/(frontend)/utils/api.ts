a32b36a59ca27ae06c98ccbb84de6db35974f3ae
vaish567 <vaishnav.parte@gmail.com>
2025-06-30 17:11:17 +0530

    update on api.ts

--- src/app/(frontend)/utils/api.ts
+++ src/app/(frontend)/utils/api.ts
@@ -169,5 +169,5 @@ export const fetchAllProjects = async (): Promise<ProjectOverview[]> => {
 
 //Fetch Service Cards
 export const fetchServiceCards = async (): Promise<ServiceCard[]> => {
-  return await fetchFromAPI('services', { depth: '2' })
+  return await fetchFromAPI('services', { depth: '1' })
 }
