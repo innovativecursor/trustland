347251556ea30b7d539c092cb4dda392bcb44f16
vaish567 <vaishnav.parte@gmail.com>
2025-06-30 10:51:02 +0530

    update on features

--- src/app/(frontend)/properties/[slug]/page.tsx
+++ src/app/(frontend)/properties/[slug]/page.tsx
@@ -5,8 +5,14 @@ import PropertyDetails from '../../components/ui/PropertyDetails'
 import FeatureSection from '../../components/ui/FeatureSection'
 import { fetchProjectOverviewBySlug } from '../../utils/api'
 
-export default async function PropertyPage({ params }: { params: { slug: string } }) {
-  const { slug } = params
+type PageProps = {
+  params: {
+    slug: string
+  }
+}
+
+export default async function PropertyPage(props: PageProps) {
+  const { slug } = props.params
 
   console.log('Received slug param:', slug)
 
